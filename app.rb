66adb0874bd7a58cf8983b8317519225983dc04a
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-19 05:00:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ module FormsLab
     end 
     
     post '/pirates' do 
-      @pirate=Pirate.new(params[:name],params[:weight],params[:height])
+      @pirate=Pirate.new(params[:pirate])
       
       
       
