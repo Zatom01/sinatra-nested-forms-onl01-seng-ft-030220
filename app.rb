90c60d69874c7441445dcd80be0ee6773cc188cd
Anil Gurung <anil_gurung15@yahoo.com>
2020-04-19 04:27:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    
+    get '/' do 
+      erb :root
+    end 
+    
+    post '/new' do 
+      erb :'pirates/new' 
+    end 
 
-    # code other routes/actions here
+    
 
   end
 end
